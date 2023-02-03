d74d104bbcc9b2315e2283d0ae752ce42a107022
caglaakin <114911261+caglaakin@users.noreply.github.com>
2023-02-03 15:58:54 +0300

    Add files via upload

---
+++ asalSayi/src/asalSayi/Main.java
@@ -0,0 +1,22 @@
+package asalSayi;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		for(int i = 2; i <=100; i++) {
+			int asal = 0;
+			for(int j=2; j<i; j++) {
+				if(i % j == 0) {
+					asal++;
+				}
+			}
+			if(asal == 0) {
+				System.out.print(i + " ");
+			}
+				
+		}
+	}
+
+}
+
